5a9003a610642e7baf02dec481c1fdcd08a624f6
Cheryl Allen <cyallen620@outlook.com>
2026-01-02 21:01:14 +0000

    maco test

--- models/date_dim.sql
+++ models/date_dim.sql
@@ -13,13 +13,13 @@ THEN 'WEEKEND'
 ELSE'BUSINESSDAY'
 END AS DAY_TYPE,
 
-{{(get_season('STARTED_AT'))}} AS SEASON_OF_YEAR
+{{get_season('STARTED_AT')}} AS SEASON_OF_YEAR
  
 FROM 
 {{ source('DEMO', 'BIKE') }}
 WHERE STARTED_AT != 'started_at'
 
-)
 
+)
 
 select * from CTE
