fb85c0d22e43a0fd9e8e1988a9bd5291eca111a1
Cheryl Allen <cyallen620@outlook.com>
2026-01-05 20:55:07 +0000

    no to)timestamp

--- macros/date_utilities.sql
+++ macros/date_utilities.sql
@@ -1,22 +1,17 @@
 
-{% macro get_season(x) %}
-
-CASE WHEN MONTH(TO_TIMESTAMP({{x}})) IN (12,1,2)
-    THEN 'WINTER'
-    WHEN MONTH(TO_TIMESTAMP({{x}})) IN (3,4,5)
-    THEN 'SPRING'
-    WHEN MONTH(TO_TIMESTAMP({{x}})) IN (6,7,8)
-    THEN 'SUMMER'
-    ELSE 'AUTUMN'
+-- macros/date_util.sql
+{% macro get_season(date_column) %}
+    CASE 
+        WHEN EXTRACT(MONTH FROM {{ date_column }}) IN (12, 1, 2) THEN 'WINTER'
+        WHEN EXTRACT(MONTH FROM {{ date_column }}) IN (3, 4, 5) THEN 'SPRING'
+        WHEN EXTRACT(MONTH FROM {{ date_column }}) IN (6, 7, 8) THEN 'SUMMER'
+        ELSE 'AUTUMN'
     END
-
 {% endmacro %}
 
-{% macro date_type(x) %}
-CASE
-WHEN DAYNAME{{(TO_TIMESTAMP(x))}} IN ('Sat', 'Sun')
-THEN 'WEEKEND'
-ELSE'BUSINESSDAY'
-END
+{% macro date_type(date_column) %}
+    CASE
+        WHEN DAYNAME({{ date_column }}) IN ('Sat', 'Sun') THEN 'WEEKEND'
+        ELSE 'BUSINESSDAY'
+    END
 {% endmacro %}
-
