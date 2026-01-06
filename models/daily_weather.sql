31950576766739a7403cc0322ddd3f6c8ba200ae
Cheryl Allen <cyallen620@outlook.com>
2026-01-06 20:20:24 +0000

    33

--- models/daily_weather.sql
+++ models/daily_weather.sql
@@ -1,9 +1,38 @@
-WITH daily_weather as (
+WITH daily_weather AS (
+    SELECT
+        DATE(time) AS daily_weather,
+        weather
+    FROM {{ source('DEMO', 'WEATHER') }}
+    WHERE cityname IS NOT NULL
+),
 
-SELECT *
-FROM {{ source('DEMO', 'WEATHER')}}
-WHERE CITYNAME IS NOT NULL
-limit 100
+weather_counts AS (
+    SELECT
+        daily_weather,
+        weather,
+        COUNT(*) AS weather_count
+    FROM daily_weather
+    GROUP BY daily_weather, weather
+),
+
+ranked_weather AS (
+    SELECT
+        daily_weather,
+        weather,
+        count(distinct weather),
+        ROW_NUMBER() OVER (
+            PARTITION BY daily_weather
+            ORDER BY weather_count DESC
+        ) AS row_number
+    FROM weather_counts
+),
+
+final AS (
+    SELECT *
+    FROM ranked_weather
 )
 
-select * from daily_weather
+SELECT *
+FROM final
+
+
